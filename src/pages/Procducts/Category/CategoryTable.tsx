06ad573f2ff97f07f4a36d034e3a60946ea81921
은정 <scpark6021@gmail.com>
2023-05-25 10:52:38 +0900

    [Feat]: 카테고리 토글

--- src/pages/Procducts/Category/CategoryTable.tsx
+++ src/pages/Procducts/Category/CategoryTable.tsx
@@ -1,17 +1,46 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
-import { Card, CardHeader, Box, Button, Divider } from "@mui/material";
-import PropTypes from "prop-types";
+import {
+  Card,
+  CardHeader,
+  Box,
+  Button,
+  Divider,
+  List,
+  ListItemButton,
+  ListItemText,
+  Collapse,
+} from "@mui/material";
 
-import { mainCategory } from "@src/types/product";
+import { MainCategory } from "@src/types/product";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
 interface RecentOrdersTableProps {
   className?: string;
-  categories: mainCategory[];
+  categories: MainCategory[];
 }
 
 const CategoryCard: FC<RecentOrdersTableProps> = ({ categories }) => {
+  const openObject: { [key: number]: boolean } = categories.reduce(
+    (obj, category) => {
+      obj[category.id] = true;
+      return obj;
+    },
+    {}
+  );
+
+  const [open, setOpen] = useState(openObject);
+
+  const showSubCategory = (categoryId: number) => {
+    setOpen((prevOpen) => ({
+      ...prevOpen,
+      [categoryId]: !prevOpen[categoryId as keyof typeof prevOpen],
+    }));
+  };
+
   return (
     <Card>
       <Box
@@ -26,8 +55,37 @@ const CategoryCard: FC<RecentOrdersTableProps> = ({ categories }) => {
         </Button>
       </Box>
       <Divider />
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        <ListItemButton>
+          <ListItemText primary="전체선택" />
+        </ListItemButton>
+        {categories.map((category) => (
+          <div key={category.id}>
+            <ListItemButton>
+              <ListItemText primary={category.name} />
+              {open[category.id] ? (
+                <ExpandLess onClick={() => showSubCategory(category.id)} />
+              ) : (
+                <ExpandMore onClick={() => showSubCategory(category.id)} />
+              )}
+            </ListItemButton>
+            <Collapse in={open[category.id]} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                {category.children?.map((subCategory) => (
+                  <ListItemButton key={subCategory.id} sx={{ pl: 4 }}>
+                    <ListItemText primary={subCategory.name} />
+                  </ListItemButton>
+                ))}
+              </List>
+            </Collapse>
+          </div>
+        ))}
+      </List>
     </Card>
   );
 };
-
 export default CategoryCard;
